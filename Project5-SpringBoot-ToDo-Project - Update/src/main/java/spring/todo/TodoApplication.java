2ad920cbeb8b2c1f11caa5fe75521a2072097a57
Urunov <hamdamboy.urunov@gmail.com>
2020-05-28 17:59:23 +0900

    Spring Boot Project JSP MySQL Controller Logic

---
+++ Project5-SpringBoot-ToDo-Project - Update/src/main/java/spring/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package spring.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+}
+//https://www.codejava.net/frameworks/spring/14-tips-for-writing-spring-mvc-controller#RequestMethod
