be8da2876748f101eb197aeb32d130364ffbeeae
Urunov <hamdamboy.urunov@gmail.com>
2020-06-03 17:49:24 +0900

    Spring MySQL Hibernate One2One Environment ongoing

---
+++ Project7-SpringHibernateOne2One/src/main/java/spring/database/DatabaseApplication.java
@@ -0,0 +1,20 @@
+package spring.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseApplication.class, args);
+    }
+
+}
+//https://github.com/callicoder/spring-boot-react-oauth2-social-login-demo
+//https://www.callicoder.com/hibernate-spring-boot-jpa-one-to-one-mapping-example/
+
+
+//https://github.com/hendisantika?tab=repositories
+
+//https://www.callicoder.com/hibernate-spring-boot-jpa-one-to-one-mapping-example/
