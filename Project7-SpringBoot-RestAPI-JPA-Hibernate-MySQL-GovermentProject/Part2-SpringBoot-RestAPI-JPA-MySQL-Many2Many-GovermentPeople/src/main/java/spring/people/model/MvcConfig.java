d3c2a65312c20354c8d82cd4b716383d32d9ae4c
Urunov <hamdamboy.urunov@gmail.com>
2020-06-15 18:30:55 +0900

    Done: Spring Boot JPA Hibernate Search Element from DB

---
+++ Project7-SpringBoot-RestAPI-JPA-Hibernate-MySQL-GovermentProject/Part2-SpringBoot-RestAPI-JPA-MySQL-Many2Many-GovermentPeople/src/main/java/spring/people/model/MvcConfig.java
@@ -0,0 +1,9 @@
+package spring.people.model;
+
+/**
+ * @Created 15 / 06 / 2020 - 6:17 PM
+ * @project SpringBootPeople
+ * @Author Hamdamboy
+ */
+public class MvcConfig {
+}
---
+++ Project7-SpringBoot-RestAPI-JPA-Hibernate-MySQL-GovermentProject/Part2-SpringBoot-RestAPI-JPA-MySQL-Many2Many-GovermentPeople/src/main/java/spring/people/service/PeopleService.java
@@ -0,0 +1,9 @@
+package spring.people.service;
+
+/**
+ * @Created 15 / 06 / 2020 - 5:59 PM
+ * @project SpringBootPeople
+ * @Author Hamdamboy
+ */
+public class PeopleService {
+}
